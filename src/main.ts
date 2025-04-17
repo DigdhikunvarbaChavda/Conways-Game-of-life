1909e9bc9e5bf3ac0becdb16a201d6bc65fba621
DigdhiC <digdhi@raapidinc.com>
2025-04-17 08:56:15 +0530

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -22,9 +22,11 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   initialLiveCells = [
-    2, 4, 7, 11, 13, 22, 24, 31, 34, 42, 47, 53, 56, 62, 66, 71, 77, 81, 88, 99,
+    2, 4, 7, 13, 22, 24, 31, 34, 42, 47, 53, 56, 62, 66, 71, 77, 81, 88, 99,
   ];
-  dimention = new Array(100).fill(0);
+
+  gridDimention = 10;
+  dimention = new Array(this.gridDimention * this.gridDimention).fill(0);
 
   name = 'Digdhi Chavda';
 
@@ -47,6 +49,7 @@ export class App {
     for (let i = 0; i < 100; i++) {
       const neighborCellCount = this.getLiveNeighborCellCount(i);
       const isCurrentCellLive = this.dimention[i];
+      console.log(isCurrentCellLive, neighborCellCount);
       if (
         isCurrentCellLive &&
         (neighborCellCount === 2 || neighborCellCount === 3)
@@ -58,19 +61,43 @@ export class App {
         updatedGrid[i] = 0;
       }
     }
-    console.log(updatedGrid);
     return updatedGrid;
   }
+
   getLiveNeighborCellCount(index: number) {
     let neighborLiveCell = 0;
-    const gridDimentions = 10;
+    const currentCellRow = Math.floor(index / this.gridDimention);
+    const currentCellColumn = index % this.gridDimention;
     for (let i = -1; i < 2; i++) {
-      if (this.dimention[index + i]) neighborLiveCell++;
-      if (this.dimention[index - gridDimentions + i]) neighborLiveCell++;
-      if (this.dimention[index + gridDimentions + i]) neighborLiveCell++;
+      for (let j = -1; j < 2; j++) {
+        const neighborRow = currentCellRow + i;
+        const neighborColumn = currentCellColumn + j;
+        if (
+          (i == 0 && j == 0) ||
+          neighborRow < 0 ||
+          neighborRow >= this.gridDimention ||
+          neighborColumn < 0 ||
+          neighborColumn >= this.gridDimention
+        ) {
+          continue;
+        }
+
+        if (this.dimention[neighborRow * this.gridDimention + neighborColumn]) {
+          neighborLiveCell++;
+        }
+      }
     }
     return neighborLiveCell;
   }
+  // getLiveNeighborCellCount(index: number) {
+  //   let neighborLiveCell = 0;
+  //   for (let i = -1; i < 2; i++) {
+  //     if (i != 0 && this.dimention[index + i]) neighborLiveCell++;
+  //     if (this.dimention[index - this.gridDimention + i]) neighborLiveCell++;
+  //     if (this.dimention[index + this.gridDimention + i]) neighborLiveCell++;
+  //   }
+  //   return neighborLiveCell;
+  // }
 
   ngOnDestroy() {
     this.updateCells$.unsubscribe();
